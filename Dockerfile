c95aaa5e02e267f7aeb0d68f337cdbdd29365b4d
seokjoon <seokjoon@gmail.com>
2023-09-22 18:05:04 +0900

    dockerize, port

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ ENTRYPOINT ["java", "-jar", "/app.jar"]
 
 
 # gradle > tasks > build > bootJar
-# docker build -t sp-boot/test:1 ./doc/docker
+# docker build -t sp-boot/test:1 .
 # docker run -d --name sp-boot-test -p 8600:8500 sp-boot/test:1
--- src/main/java/spring/boot/test/springboottest2/SpringBootTest2Application.java
+++ src/main/java/spring/boot/test/springboottest2/SpringBootTest2Application.java
@@ -2,23 +2,16 @@ package spring.boot.test.springboottest2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@Controller
 @SpringBootApplication
 public class SpringBootTest2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTest2Application.class, args);
     }
-
-//    @RequestMapping("/")
-//    String index() {
-//        return "index.html";
-//    }
 
     @RequestMapping("/foo")
     String foo() {
