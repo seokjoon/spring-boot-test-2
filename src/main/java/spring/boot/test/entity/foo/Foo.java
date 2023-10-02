6f8b354465d8605d8754135c9ea33dae3ee58d31
seokjoon <seokjoon@gmail.com>
2023-10-02 14:58:17 +0900

    api crud

--- src/main/java/spring/boot/test/entity/foo/Foo.java
+++ src/main/java/spring/boot/test/entity/foo/Foo.java
@@ -5,6 +5,10 @@ import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
@@ -16,12 +20,19 @@ public class Foo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    @CreatedDate
+    public LocalDateTime createdAt;
+
 //    @Column(name = "num")
     @Min(value = 2, message = "num minimum 2")
     public int num = 30;
 
     @Column(nullable = false)
     public String title = "default title";
+
+    @LastModifiedDate
+    public LocalDateTime updatedAt;
+
 
     @Builder
     public Foo(int num, String title) {
--- src/main/java/spring/boot/test/res/foo/FooRes.java
+++ src/main/java/spring/boot/test/res/foo/FooRes.java
@@ -2,16 +2,22 @@ package spring.boot.test.res.foo;
 import lombok.Getter;
 import spring.boot.test.entity.foo.Foo;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class FooRes {
 
+    private final LocalDateTime createdAt;
     private final long id;
     private final int num;
     private final String title;
+    private final LocalDateTime updatedAt;
 
     public FooRes(Foo foo) {
+        this.createdAt = foo.createdAt;
         this.id = foo.id;
         this.num = foo.num;
         this.title = foo.title;
+        this.updatedAt = foo.updatedAt;
     }
 }
